06cba939acd392ab9902aead8745c52be48797fa
Sathish <sathish.g@stackroute.in>
2022-04-13 10:56:50 +0530

    Code Repository / Source Code

---
+++ src/com/wipro/calc/Test.java
@@ -0,0 +1,38 @@
+package com.wipro.calc;
+
+public class Test {
+	
+	
+	public static void main(String[] xyz ) {
+		// Variable -  Used to Store the Value
+		int x=100;  
+		// Value can be changed during execution of program
+		x=x+10;
+		
+		// DataType - Eight Primitive DataTypes
+		byte age=18;  // 1-100  1 byte
+		short salary=20000; // 2 byte
+		int pincode= 635112;  //  4 byte
+		long phone=9566259956l; // 8 byte
+		
+		int age1=18;  // 3 byte waste
+		
+		float weight=12.34f;  // 4 byte
+		double height=14.34;  // 8 byte 
+		
+		boolean status=true;
+		
+		char type = 'A'; // 2 byte
+		
+		// Non Primitive datatypes
+		String company="Wipro"; // Group of Characters enclosed within double quotes
+		// Array Class Object
+		
+		
+		
+		
+		
+		
+	}
+
+}
